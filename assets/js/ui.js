197dd723a62c64aea2bfbdc16b48677f9eba6457
Gustavo Arancibia <garancibiacl@gmail.com>
2025-07-02 17:00:03 -0400

    se agrega funcionalidad toggle

--- assets/js/ui.js
+++ assets/js/ui.js
@@ -65,3 +65,25 @@ export function renderLayouts() {
     });
   }
  // FIN FUNCION TOAST
+
+
+ document.addEventListener("DOMContentLoaded", () => {
+  const toggle = document.getElementById("themeToggle");
+
+  // Verifica si hay un tema guardado
+  const savedTheme = localStorage.getItem("theme");
+  if (savedTheme === "light") {
+    document.body.classList.add("light-theme");
+    toggle.checked = true;
+  }
+
+  toggle.addEventListener("change", () => {
+    if (toggle.checked) {
+      document.body.classList.add("light-theme");
+      localStorage.setItem("theme", "light");
+    } else {
+      document.body.classList.remove("light-theme");
+      localStorage.setItem("theme", "dark");
+    }
+  });
+});
